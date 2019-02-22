cbe1a71f834077738d5a319174300539bfacc717
Ravi Mahfunda <ravimahfunda@gmail.com>
2019-02-22 18:02:31 +0700

    adding jump and spin

---
+++ Assets/Scripts/Clock/ClockArmBehaviours.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockArmBehaviours : MonoBehaviour
+{
+    public float spinSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, spinSpeed * Time.deltaTime,0));
+    }
+}
---
+++ Assets/Scripts/Player/PlayerBehaviours.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviours : MonoBehaviour
+{
+    public GameObject fractile;
+
+    public float jumpPower;
+    public bool allowDoubleJump;
+
+    private bool grounded = true;
+    private bool isGrounded;
+    private int jumpCount;
+    private const int jumpLimit = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
+
+    void Jump() {
+        Debug.Log("Jump");
+        if (grounded == true || (allowDoubleJump && jumpCount < jumpLimit))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            grounded = false;
+            jumpCount++;
+        }
+    }
+
+    void Die() {
+        Debug.Log("Die");
+        Instantiate(fractile, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Arm")) {
+            Die();
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            jumpCount = 0;
+        }
+    }
+
+
+}
